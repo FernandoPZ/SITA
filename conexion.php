2a9825a49812ad4b3422000bfd21f90f0e9b7e8d
Fernando <50506780+FernandoPZ@users.noreply.github.com>
2022-05-19 10:00:26 -0500

    Corrección del error

--- conexion.php
+++ conexion.php
@@ -5,9 +5,9 @@ $user = 'root'; //Guarda el nombre del usuario
 $password = ''; //Guarda la contraseña
 $db = 'sita'; //Guarda el nombre de la base de datos
 
-$conection = @mysqli_connect($host,$user,$password,$db); //Realiza la coneccion
+$conexion = @mysqli_connect($host,$user,$password,$db); //Realiza la coneccion
 
-if(!$coneccion){ //Sentencia "IF" para saber si se conecta
+if(!$conexion){ //Sentencia "IF" para saber si se conecta
     echo "Error en la coneccion :("; 
 }else{
     echo "Coneccion exitosa :D";
