511f0b33a33c5c1b7f26f2dc57f0b81c9a2625cd
Fernando <50506780+FernandoPZ@users.noreply.github.com>
2022-05-23 09:05:22 -0500

    Delete sistema directory

--- sistema/index.php
+++
@@ -1,9 +0,0 @@
-<?php include("template/cabecera.php"); ?>
-
-			<div class="jumbotron">
-				<h1 class="display-3">Pagina principal</h1>
-				<p class="lead">Aqui falta contenido</p>
-				<hr class="my-2">
-			</div>
-
-<?php include("template/pie.php"); ?>
