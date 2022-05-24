5fb6b491b5bb70bf89609006133cc4d5fde39cbd
Fernando <50506780+FernandoPZ@users.noreply.github.com>
2022-05-24 10:51:33 -0500

    Agregar usuarios funcional

--- sistema/secciones/nuevoUsuario.php
+++ sistema/secciones/nuevoUsuario.php
@@ -1,46 +1,68 @@
 <?php
-if(!empty($_POST))
-{
-    $alert='';
-    if(empty($_POST['usuario']) || empty($_POST['tipou']) || empty($_POST['contra01']) || empty($_POST['contra02']))
-    {
-        $alert="
-        <div class="alert alert-dismissible alert-warning">
-            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-            <strong>Se te olvida algo...</strong> debes de llenar todos los campos.
-        </div>
-        ";
-    }else{
-        include "../config/conexion.php";
+$decision=(isset($_POST['decision']))?$_POST['decision']:""; //Boton de decision
+include "../config/conexion.php";
+?>
 
-        $usuario = $_POST['usuario'];
-        $tipou = $_POST['tipou'];
-        $contra1 = $_POST['contra01'];
-        $contra2 = $_POST['contra02'];
+<?php
+switch($decision){
 
-        $query = mysqli_query($conexion,"SELECT * FROM usuario WHERE usuario = '$usuario'";)
-        $result = mysqli_fetch_array($query);
-
-        if($result > 0){
-            $alert="
-            <div class="alert alert-dismissible alert-warning">
-                <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-                <strong>Oh vaya...</strong> el nombre de usuario introducido ya esta ocupado, escoge otro.
-            </div>
-            ";
-        }else{
-            if($contra1 == $contra2){
-                $alert="
-                <div class="alert alert-dismissible alert-danger">
-                    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-                    <strong>Oh vaya...</strong> las contraseñas no coinciden.
+    case "guardar":
+        if(!empty($_POST))
+        {
+            $alert='';
+            if(empty($_POST['usuario']) || empty($_POST['tipou']) || empty($_POST['contra01']) || empty($_POST['contra02']))
+            {
+                $alert='
+                <div class="alert alert-dismissible alert-warning">
+                    <strong>Se te olvida algo...</strong> debes de llenar todos los campos.
                 </div>
-                ";
+                ';
             }else{
-                $query_insert = mysqli_query($conexion,"INSERT INTO usuario(cve_tipou,usuario,pass,activo) VALUES ()")
+                $usuario = $_POST['usuario'];
+                $tipou = $_POST['tipou'];
+                $contra1 = $_POST['contra01'];
+                $contra2 = $_POST['contra02'];
+
+                $query = mysqli_query($conexion,"SELECT * FROM usuario WHERE usuario = '$usuario'");
+                $result = mysqli_fetch_array($query);
+
+                if($result > 0){
+                    $alert='
+                    <div class="alert alert-dismissible alert-warning">
+                        <strong>Oh vaya...</strong> el nombre de usuario introducido ya esta ocupado, escoge otro.
+                    </div>
+                    ';
+                }else{
+                    if($contra1 != $contra2){
+                        $alert='
+                        <div class="alert alert-dismissible alert-danger">
+                            <strong>Oh vaya...</strong> las contraseñas no coinciden.
+                        </div>
+                        ';
+                    }else{
+                        $query_insert = mysqli_query($conexion,"INSERT INTO usuario(cve_tipou,usuario,pass,activo) VALUES ('$tipou','$usuario',md5('$contra1'),'Si')");
+                        if($query_insert){
+                            $alert='
+                                <div class="alert alert-dismissible alert-success">
+                                    <strong>Listo!</strong> El usuario se guardo correctamente.
+                                </div>
+                            ';
+                        }else{
+                            $alert='
+                                <div class="alert alert-dismissible alert-danger">
+                                    <strong>Algo salio mal...</strong> El usuario no se pudo guardar.
+                                </div>
+                            ';
+                        }
+                    }
+                }
             }
         }
-    }
+    break;
+
+    case "cancelar":
+        header('Location:/SITA/sistema/secciones/verUsuario.php');
+    break;
 }
 ?>
 
@@ -50,7 +72,8 @@ if(!empty($_POST))
 			<div class="jumbotron">
 				<h1 class="display-3">Registrar nuevo usuario</h1>
                 <hr class="my-2">
-                    <br>
+                <br>
+                <form action="" method="POST">
                     <div class="card">
                         <div class="card-header text-center">
                             Llene el siguiente formulario
@@ -60,11 +83,21 @@ if(!empty($_POST))
                                 <label class="form-label mt-2">Nombre de usuario</label>
                                 <input type="text" class="form-control" name="usuario" placeholder="usuario">
                                 <label class="form-label mt-2">Tipo de usuario</label>
+                                <?php
+                                    $query_tipou = mysqli_query($conexion,"SELECT * FROM tipo_usuario");
+                                    $result_tipou = mysqli_num_rows($query_tipou);
+                                ?>
                                 <select class="form-select" name="tipou" id="tipou">
-                                    <option value="1">Administrador</option>
-                                    <option value="2">Consultor</option>
-                                    <option value="3">Academico</option>
-                                    <option value="4">Aspirante</option>
+                                    <?php
+                                        if($result_tipou > 0)
+                                        {
+                                            while ($tipou = mysqli_fetch_array($query_tipou)){
+                                                ?>
+                                                <option value="<?php echo $tipou["cve_tipou"]; ?>"><?php echo $tipou["nombre"]; ?></option>
+                                                <?php
+                                            }
+                                        }
+                                    ?>
                                 </select>
                                 <label class="form-label mt-2">Escriba su contraseña</label>
                                 <input type="password" class="form-control" name="contra01" placeholder="*******">
@@ -72,11 +105,13 @@ if(!empty($_POST))
                                 <input type="text" class="form-control" name="contra02" placeholder="contraseña">
                             </div>
                             <div class="text-center">
-                                <button type="submit" name="accion" value="entrar" class="btn btn-save btn-primary">Guardar</button>
-                                <button onclick="location.href='/SITA/sistema/secciones/verUsuario.php'" class="btn btn-danger">Cancelar</button>
+                                <div class="alert"><?php echo isset($alert) ? $alert : ''; ?></div>
+                                <button type="submit" name="decision" value="guardar" class="btn btn-primary" style="float: left;">Guardar</button>
+                                <button type="submit" name="decision" value="cancelar" class="btn btn-danger" style="float: right;">Cancelar</button>
                             </div>
                         </div>
                     </div>
-			</div>
+                </form>
+            </div>
 
 <?php include("../template/pie.php"); ?>
