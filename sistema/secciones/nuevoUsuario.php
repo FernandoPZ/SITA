0b46a7b586776f90562c984b7387693216200d0c
Fernando <50506780+FernandoPZ@users.noreply.github.com>
2022-06-22 11:06:17 -0500

    Pagina[nuevoUsuario] (Prototipo) Listo

--- sistema/secciones/nuevoUsuario.php
+++ sistema/secciones/nuevoUsuario.php
@@ -1,79 +1,74 @@
-<?php include("../template/cabecera.php"); ?>
+<?php include("../template/cabecera.php"); ?> <!-- Llama a la cabecera -->
 
 <?php
-if($_SESSION['tipo'] != 1)
+if($_SESSION['tipo'] != 1) // Valida si el usuario es nivel administrador
 {
-    header("location: /SITA/sistema/index.php");
+    header("location: /SITA/sistema/index.php"); // Redirecciona a la pagina pricipal
 }
-
-$decision=(isset($_POST['decision']))?$_POST['decision']:""; //Boton de decision
-include "../config/conexion.php";
+$decision=(isset($_POST['decision']))?$_POST['decision']:""; // Boton de decision
+include "../config/conexion.php"; // Realiza la coneccion de la bd
 ?>
 
 <?php
 switch($decision){
-
-    case "guardar":
-        if(!empty($_POST))
+    case "guardar": // Guargar
+        if(!empty($_POST)) // Valida si los campos no esten vacios
         {
             $alert='';
-            if(empty($_POST['tipo'])
-            || empty($_POST['nombre'])
-            || empty($_POST['apellido1'])
-            || empty($_POST['apellido2'])
-            || empty($_FILES['foto'])
-            || empty($_POST['usuario'])
-            || empty($_POST['pass1'])
-            || empty($_POST['pass2'])
-            || empty($_POST['correo']))
+            if(empty($_POST['tipo']) // Tipo de usuario
+            || empty($_POST['nombre']) // Nombre del usuario
+            || empty($_POST['apellido1']) // Primer apellido del usuario
+            || empty($_POST['apellido2']) // Segundo apellido del usuario
+            || empty($_FILES['foto']) // Foto del usuario
+            || empty($_POST['usuario']) // ID del usuario
+            || empty($_POST['pass1']) // Contraseña
+            || empty($_POST['pass2']) // Contaseña (verificacion)
+            || empty($_POST['correo'])) // Correo del usuario
             {
-                $alert='
+                $alert=' 
                 <div class="alert alert-dismissible alert-warning">
                     <strong>Se te olvida algo...</strong> debes de llenar todos los campos.
                 </div>
-                ';
+                '; // Alerta de algun campo vacio
             }else{
-                $tipo = $_POST['tipo'];
-                $nombre = $_POST['nombre'];
-                $apellido1 = $_POST['apellido1'];
-                $apellido2 = $_POST['apellido2'];
-                $foto = $_FILES['foto']['name']; //Foto
-                $usuario = $_POST['usuario'];
-                $pass1 = $_POST['pass1'];
-                $pass2 = $_POST['pass2'];
-                $correo = $_POST['correo'];
-
+                $tipo = $_POST['tipo']; // Guarda el tipo de usuario
+                $nombre = $_POST['nombre']; // Guarda el nombre del usuario
+                $apellido1 = $_POST['apellido1']; // Guarda el primer apellido del usuario
+                $apellido2 = $_POST['apellido2']; // Guarda el segundo apellido del usuario
+                $foto = $_FILES['foto']['name']; // Guarda la foto del usuario
+                $usuario = $_POST['usuario']; // Guarda el ID del usuario
+                $pass1 = $_POST['pass1']; // Guarda la contraseña del usuario
+                $pass2 = $_POST['pass2']; // Guarda la verificacion de contraseña del usuario
+                $correo = $_POST['correo']; // Guarda el correo del usuario
                 // Asignacion de nombre unico a la foto
-                $fecha= new DateTime();
-                $nombreFoto=($foto!="")?$usuario."_".$fecha->getTimestamp()."_".$foto:"default.png";
-
-                $query = mysqli_query($conexion,"SELECT * FROM usuario WHERE usuario = '$usuario'");
-                $result_usu = mysqli_fetch_array($query);
-
-                $query = mysqli_query($conexion,"SELECT * FROM usuario WHERE correo = '$correo'");
-                $result_cor = mysqli_fetch_array($query);
-
-                if($result_usu > 0){
+                $fecha= new DateTime(); // Determina la fecha actual
+                $nombreFoto=($foto!="")?$usuario."_".$fecha->getTimestamp()."_".$foto:"default.png"; // Nuevo nombre
+                $query = mysqli_query($conexion,"SELECT * FROM usuario WHERE usuario = '$usuario'"); // Verifica que el usuario introducido este en la bd
+                $result_usu = mysqli_fetch_array($query); // Almacena cuantas coincidencias existen
+                $query = mysqli_query($conexion,"SELECT * FROM usuario WHERE correo = '$correo'"); // Verifica que el correo introducido este en la bd
+                $result_cor = mysqli_fetch_array($query); // Almacena cuantas coincidencias existen
+                if($result_usu > 0){ // Si hay alguna coincidencia con el usuario, muestra la alerta
                     $alert='
                     <div class="alert alert-dismissible alert-warning">
                         <strong>Oh vaya...</strong> el nombre de usuario introducido ya esta registrado, escoge otro.
                     </div>
-                    ';
+                    '; // Alerta de coincidencia de usuario
                 }else{
-                    if($result_cor > 0){
+                    if($result_cor > 0){ // Si hay alguna coincidencia con el correo, muestra la alerta
                         $alert='
                         <div class="alert alert-dismissible alert-warning">
                             <strong>Oh vaya...</strong> el correo introducido ya esta registrado, escoge otro.
                         </div>
-                        ';
+                        '; // Alerta de coincidencia de correo
                     }else{
-                        if($pass1 != $pass2){
+                        if($pass1 != $pass2){ // Si las contraseñas no son las mismas
                             $alert='
                             <div class="alert alert-dismissible alert-danger">
                                 <strong>Oh vaya...</strong> las contraseñas no coinciden.
                             </div>
-                            ';
+                            '; // Muestra la alerta de contraseñas diferentes
                         }else{
+                            // Inserta los datos en la tabla 
                             $query_insert = mysqli_query($conexion,"INSERT INTO usuario(tipo,nombre,apellido1,apellido2,foto,usuario,pass,correo)
                                                                                 VALUES ('$tipo','$nombre','$apellido1','$apellido2','$nombreFoto','$usuario',md5('$pass1'),'$correo')");
                             if($query_insert){
@@ -81,43 +76,43 @@ switch($decision){
                                     <div class="alert alert-dismissible alert-success">
                                         <strong>Listo!</strong> El usuario se guardo correctamente.
                                     </div>
-                                ';
-                                $archivoFoto=$_FILES["foto"]["tmp_name"];
-                                if($archivoFoto!=""){
-                                    move_uploaded_file($archivoFoto,"../files/upload/fotos/".$nombreFoto);
+                                '; // Alerta de que se guardo correctamente
+                                $archivoFoto=$_FILES["foto"]["tmp_name"]; // Almacena la imagen subida
+                                if($archivoFoto!=""){ // Verifica que el campo de subida no este vacio
+                                    move_uploaded_file($archivoFoto,"../files/upload/fotos/".$nombreFoto); // Mueve la imagen subida a otra carpeta dentro del sistema
                                 }
                             }else{
                                 $alert='
                                     <div class="alert alert-dismissible alert-danger">
                                         <strong>Algo salio mal...</strong> El usuario no se pudo guardar.
                                     </div>
-                                ';
+                                '; // Alerta de algun problema al guardar el registro
                             }
                         }
                     }
                 }
-                mysqli_close($conexion);
+                mysqli_close($conexion); // Cierra conexion con la bd
             }
             break;
         }
 
-    case "cancelar":
-        header('Location:/SITA/sistema/secciones/verUsuario.php');
-        mysqli_close($conexion);
+    case "cancelar": // Cancelar
+        header('Location:/SITA/sistema/secciones/verUsuario.php'); // Redirecciona a la lista de los usuarios
+        mysqli_close($conexion); // Cierra conexion con la bd
     break;
 }
 ?>
 
-<title>SITA - Registrar usuario</title>
+<title>SITA - Registrar usuario</title> <!-- Llama al encabezado -->
 
-			<div class="jumbotron">
-				<h1 class="display-3">Registrar nuevo usuario</h1>
+            <div class="jumbotron">
+                <h1 class="display-3">Registrar nuevo usuario</h1>
                 <hr class="my-2">
                 <br>
-                <form action="" method="POST" enctype="multipart/form-data">
+                <form action="" method="POST" enctype="multipart/form-data"> <!-- "enctype" necesario para poder reconocer los archivos subidos -->
                     <div class="card">
                         <div class="card-header text-center">
-                            Llene el siguiente formulario
+                            <h4>Llene el siguiente formulario</h4>
                         </div>
                         <div class="card-body">
                                 <div class="row">
@@ -142,16 +137,16 @@ switch($decision){
                                     <div class = "form-group col-md-6 mb-3">
                                         <label class="form-label mt-2">Tipo de usuario</label>
                                         <?php
-                                            include "../config/conexion.php";
-                                            $query_tipou = mysqli_query($conexion,"SELECT * FROM tipo_usuario");
-                                            mysqli_close($conexion);
-                                            $result_tipou = mysqli_num_rows($query_tipou);
+                                            include "../config/conexion.php"; // Realiza la conexion con la bd
+                                            $query_tipou = mysqli_query($conexion,"SELECT * FROM tipo_usuario"); // Consulta todos los tipos de usuarios
+                                            mysqli_close($conexion); // Cierra conexion
+                                            $result_tipou = mysqli_num_rows($query_tipou); // Almacena la cantidad de registros en la consulta
                                         ?>
                                         <select class="form-select" name="tipo" id="tipo">
                                             <?php
-                                                if($result_tipou > 0)
+                                                if($result_tipou > 0) // Valida si hay registros
                                                 {
-                                                    while ($tipou = mysqli_fetch_array($query_tipou)){
+                                                    while ($tipou = mysqli_fetch_array($query_tipou)){ // Bucle para mostrar todos los registros
                                                         ?>
                                                         <option value="" hidden>Selecciona una opción</option>
                                                         <option value="<?php echo $tipou["cve_tipo_usu"]; ?>"><?php echo $tipou["tipo"]; ?></option>
@@ -177,20 +172,26 @@ switch($decision){
                                         <div class="m-0 vh-50 row justify-content-center align-items-center">
                                             <div class="col-auto">
                                                 <br>
-                                                <output id="previsual"></output>
-                                                <script> <?php include("../js/scripts.js"); ?> </script>
+                                                <output id="previsual"></output> <!-- Espacio para previsualizar la foto subida -->
+                                                <script> <?php include("../js/scripts.js"); ?> </script> <!-- llama al script necesario para poder previsualizar -->
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             <div class="text-center">
-                                <div class="alert"><?php echo isset($alert) ? $alert : ''; ?></div>
-                                <button type="submit" name="decision" value="guardar" class="btn btn-primary" style="float: left;">Guardar</button>
-                                <button type="submit" name="decision" value="cancelar" class="btn btn-danger" style="float: right;">Cancelar</button>
+                                <div class="alert"><?php echo isset($alert) ? $alert : ''; ?></div> <!-- Espacio para las alertas -->
+                                <button type="submit" name="decision" value="guardar" class="btn btn-primary" style="float: left;">Guardar</button> <!-- Guarda el registro -->
+                                <button type="submit" name="decision" value="cancelar" class="btn btn-danger" style="float: right;">Cancelar</button> <!-- Redirecciona al listado de usuarios -->
                             </div>
                         </div>
                     </div>
                 </form>
             </div>
 
-<?php include("../template/pie.php"); ?>
+<?php include("../template/pie.php"); ?> <!-- Llama al pie de la pagina -->
+
+<!--
+--- Pagina[verUsuarios] (Prototipo) ---
+Codificacion final -- [21/06/2022 (10:20 hrs)]
+Comentario final ---- [21/06/2022 (10:20 hrs)]
+-->
