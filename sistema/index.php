60f44c6cc809b14e34688ffa00f3f6f47b9ac899
Fernando <50506780+FernandoPZ@users.noreply.github.com>
2022-07-04 10:17:36 -0500

    Pagina[Home] (Prototipo) Listo
    Agregado el apartado de Docente en los usuarios

--- sistema/index.php
+++ sistema/index.php
@@ -7,43 +7,81 @@
 				<hr class="my-2">
 			</div>
 			<br>
+			<?php if($_SESSION['tipo'] == 4){ ?> <!-- Valida que el usuario es un administrador -->
+				<div class="col-md-4 mx-auto">
+					<div class="card">
+						<h3 class="card-header col text-center">Ver mi usuario</h3>
+						<div class="card-body">
+							<img class="card-img-top" src="elementos/recuhum.png" alt="">
+							<div class="col text-center">
+								</br>
+								<a name="" id="" class="btn btn-primary" href="secciones/mi_usuario.php" role="button">Entrar</a> <!-- Redirecciona para ver la lista de ususarios -->
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 mx-auto">
+					<div class="card">
+						<h3 class="card-header col text-center">Ver mi informacion</h3>
+						<div class="card-body">
+							<img class="card-img-top" src="elementos/recuhum.png" alt="">
+							<div class="col text-center">
+								</br>
+								<a name="" id="" class="btn btn-primary" href="secciones/infDocente.php" role="button">Entrar</a> <!-- Redirecciona para ver la lista de ususarios -->
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 mx-auto">
+					<div class="card">
+						<h3 class="card-header col text-center">Importar mi informacion</h3>
+						<div class="card-body">
+							<img class="card-img-top" src="elementos/recuhum.png" alt="">
+							<div class="col text-center">
+								</br>
+								<a name="" id="" class="btn btn-primary" href="secciones/inpDocente.php" role="button">Entrar</a> <!-- Redirecciona para ver la lista de ususarios -->
+							</div>
+						</div>
+					</div>
+				</div>
+			<?php } ?>
 			<!--
 				En la pagina principal solo se muestran accesos al listado
 				de los usuarios y docentes, si hay otra idea de que mas poner
 				se editara en el futuro.
 			-->
-			
-			<?php if($_SESSION['tipo'] == 1){ ?> <!-- Valida que el usuario es un administrador -->
-			<div class="col-md-4 mx-auto">
-				<div class="card">
-					<h3 class="card-header col text-center">Usuarios</h3>
-					<div class="card-body">
-						<img class="card-img-top" src="elementos/recuhum.png" alt="">
-							<div class="col text-center">
-								</br>
-								<a name="" id="" class="btn btn-primary" href="secciones/verUsuario.php" role="button">Entrar</a> <!-- Redirecciona para ver la lista de ususarios -->
+			<?php if($_SESSION['tipo'] != 4){ ?> <!-- Valida que el usuario es un administrador -->
+				<?php if($_SESSION['tipo'] == 1){ ?> <!-- Valida que el usuario es un administrador -->
+					<div class="col-md-4 mx-auto">
+						<div class="card">
+							<h3 class="card-header col text-center">Usuarios</h3>
+							<div class="card-body">
+								<img class="card-img-top" src="elementos/recuhum.png" alt="">
+									<div class="col text-center">
+										</br>
+										<a name="" id="" class="btn btn-primary" href="secciones/verUsuario.php" role="button">Entrar</a> <!-- Redirecciona para ver la lista de ususarios -->
+									</div>
 							</div>
+						</div>
+					</div>
+				<?php } ?>
+				<div class="col-md-4 mx-auto">
+					<div class="card">
+						<h3 class="card-header col text-center">Docentes</h3>
+						<div class="card-body">
+							<img class="card-img-top" src="elementos/academ.png" alt="">
+								<div class="col text-center">
+									</br>
+									<a name="" id="" class="btn btn-primary" href="secciones/verDocente.php" role="button">Entrar</a> <!-- Redirecciona para ver la lista de docentes -->
+								</div>
+						</div>
 					</div>
 				</div>
-			</div>
 			<?php } ?>
-
-			<div class="col-md-4 mx-auto">
-				<div class="card">
-					<h3 class="card-header col text-center">Docentes</h3>
-					<div class="card-body">
-						<img class="card-img-top" src="elementos/academ.png" alt="">
-							<div class="col text-center">
-								</br>
-								<a name="" id="" class="btn btn-primary" href="secciones/verDocente.php" role="button">Entrar</a> <!-- Redirecciona para ver la lista de docentes -->
-							</div>
-					</div>
-				</div>
-			</div>
 
 <?php include("template/pie.php"); ?> <!-- llamamos al pie de pagina -->
 
 <!--
 --- Pagina[Home] (Prototipo) ---
-Ultima modificacion -- [29/06/2022 (14:01 hrs)]
+Ultima modificacion -- [04/07/2022 (09:49 hrs)]
 -->
