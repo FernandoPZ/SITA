508f6da455ea814f60442bcc35da6094572cb279
Fernando <50506780+FernandoPZ@users.noreply.github.com>
2022-07-13 11:45:09 -0500

    Salir (Prototipo) Listo

--- sistema/config/salir.php
+++ sistema/config/salir.php
@@ -1,10 +1,10 @@
 <?php
-session_start(); // Inicializa la sesion
+session_start(); // Inicia la sesion
 session_destroy(); // Destruye la sesion
 header('location: ../../index.php') // Redirecciona al login
 ?>
 
 <!--
 --- Salir (Prototipo) ---
-Ultima modificacion -- [29/06/2022 (13:04 hrs)]
+Ultima modificacion -- [13/07/2022 (11:44 hrs)]
 -->
