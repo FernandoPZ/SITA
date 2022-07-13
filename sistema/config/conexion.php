96d7ef74208149adb428a0483d471e9a0d85e7c5
Fernando <50506780+FernandoPZ@users.noreply.github.com>
2022-07-13 11:38:25 -0500

    Conexión con la bd (Lista)

--- sistema/config/conexion.php
+++ sistema/config/conexion.php
@@ -3,15 +3,10 @@ $host = 'localhost'; //Guarda el nombre del host
 $user = 'root'; //Guarda el nombre del usuario
 $password = ''; //Guarda la contraseña
 $db = 'sita'; //Guarda el nombre de la base de datos
-$conexion = @mysqli_connect($host,$user,$password,$db); //Realiza la coneccion
-if(!$conexion){ //Sentencia "IF" para saber si se conecta
-    //echo "Error en la coneccion :("; 
-}else{
-    //echo "Coneccion exitosa :D";
-}
+$conexion = @mysqli_connect($host,$user,$password,$db); //Realiza la conexion
 ?>
 
 <!--
 --- Conexion con la bd ---
-Ultima modificacion -- [29/06/2022 (12:58 hrs)]
+Ultima modificacion -- [13/07/2022 (11:36 hrs)]
 -->
