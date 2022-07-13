da8f54e4b6ac93ff533a128d64c7195b7bbc4edc
Fernando <50506780+FernandoPZ@users.noreply.github.com>
2022-07-13 11:57:40 -0500

    Templates[Pie] (Prototipo) Listo

--- sistema/template/pie.php
+++ sistema/template/pie.php
@@ -1,11 +1,11 @@
         </div>
     </div>
     </br>
-    <footer class="bg-black"> <!-- Mensaje de pie de pagina -->
+    <footer class="bg-black"> <!-- Mensaje de pie de pagina (Fondo negro) -->
         <div class="container">
             <div class="row">
                 <div class="col-md-12 ">
-                    <br>
+                    <br> <!-- Salto de linea -->
                     <p class="text-center"><b>Proyecto final para obtener el titulo de Ingenieria en desarrollo y gestion de software</b></p>
                     <p class="text-center">Sistema diseñado y desarrollado por Fernando Pérez Súarez</p>
                     <p class="text-center">E-mail: f3rnandopzsz@gmail.com</p>
@@ -19,5 +19,5 @@
 
 <!--
 --- Templates[Pie] (Prototipo) ---
-Ultima modificacion -- [29/06/2022 (13:38 hrs)]
+Ultima modificacion -- [13/07/2022 (11:57 hrs)]
 -->
