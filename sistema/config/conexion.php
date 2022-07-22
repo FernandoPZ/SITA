d834ad57ef54b96db1e814b66edc6c02f4613d4d
Fernando <50506780+FernandoPZ@users.noreply.github.com>
2022-07-22 12:10:49 -0500

    Conexion con la bd Listo

--- sistema/config/conexion.php
+++ sistema/config/conexion.php
@@ -4,7 +4,6 @@ $user = 'root'; //Guarda el nombre del usuario
 $password = ''; //Guarda la contraseña
 $db = 'sita'; //Guarda el nombre de la base de datos
 $conexion = @mysqli_connect($host,$user,$password,$db); //Realiza la conexion
-
 // --- Conexion con la bd ---
-// Ultima modificacion -- [13/07/2022 (11:36 hrs)]
+// Ultima modificacion -- [22/07/2022 (12:10 hrs)]
 ?>
