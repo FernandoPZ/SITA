036776c9979921e6c950858ec6b0efff31d4acf8
Fernando <50506780+FernandoPZ@users.noreply.github.com>
2022-07-22 11:07:24 -0500

    Pagina[expDocente] (Prototipo) Listo

--- sistema/secciones/expDocente.php
+++ sistema/secciones/expDocente.php
@@ -6,15 +6,13 @@ if(empty($_GET['id'])) // Valida si la clave del usuario no esta vacia
     mysqli_close($conexion); // Cierra la conexion con la bd
 }
 $idDoc = $_GET['id']; // Almacena la clave del docente
-// Consulta todos los datos de la clave del docente
-$sql_docente = mysqli_query($conexion,"SELECT * FROM docente WHERE cve_docente = $idDoc");
-$result_sql_docente = mysqli_num_rows($sql_docente); // Almacena la cantidad todal de registros
-
+$sql_docente = mysqli_query($conexion,"SELECT * FROM docente WHERE cve_docente = $idDoc"); // Consulta todos los datos de la clave del docente
+$result_sql_docente = mysqli_num_rows($sql_docente); // Almacena la cantidad todal de registros de la tabla docente
 if($result_sql_docente == 0){ // Verifica que la cantidad no este vacia
-    $datos = 0;
+    $datos = 0; // Indicador de que no hay datos
 }else{
-    $datos = 1;
-    while ($data_docente = mysqli_fetch_array($sql_docente))
+    $datos = 1; // Identificador de que si hay datos
+    while ($data_docente = mysqli_fetch_array($sql_docente)) // Bucle para almacenar los datos de la tabla docente
     {
         $idDoc = $data_docente['cve_docente']; // Guarda la clave del docente
         $puesto = $data_docente['puesto']; // Guarda el puesto del docente
@@ -31,34 +29,32 @@ if($result_sql_docente == 0){ // Verifica que la cantidad no este vacia
         $user_cve = $data_docente['user_cve']; // Guarda la clave del usuario que lo agrego
         $activo = $data_docente['activo']; // Guarda el estatus del registro
     }
-    // Consulta todos los datos de la tabla informacion
-    $sql_informacion = mysqli_query($conexion,"SELECT * FROM informacion WHERE cve_docente = $idDoc");
-    while ($data_informacion = mysqli_fetch_array($sql_informacion))
+    $sql_informacion = mysqli_query($conexion,"SELECT * FROM informacion WHERE cve_docente = $idDoc");// Consulta todos los datos de la tabla informacion
+    while ($data_informacion = mysqli_fetch_array($sql_informacion)) // Bucle para almacenar los datos de la tabla informacion
     {
         $cve_info = $data_informacion['cve_info']; // Guarda la clave de informacion
         $fecha_nac = $data_informacion['fecha_nac']; // Guarda la fecha de nacimiento del docente
-        $doc_naca = $data_informacion['doc_nac']; // Guarda el nombre del archivo del acta de nacimiento
+        $doc_nac = $data_informacion['doc_nac']; // Guarda el nombre del archivo del acta de nacimiento
         $genero = $data_informacion['genero']; // Guarda el genero del docente
         $estado_civil = $data_informacion['estado_civil']; // Guarda el estado civil del docente
         $nacionalidad = $data_informacion['nacionalidad']; // Guarda la nacionalidad del docente
         $curp = $data_informacion['curp']; // Guarda el CURP del docente
-        $doc_curpa = $data_informacion['doc_curp']; // Guarda el nombre del archivo CURP del docente
+        $doc_curp = $data_informacion['doc_curp']; // Guarda el nombre del archivo CURP del docente
         $rfc = $data_informacion['rfc']; // Guarda el RFC del docente
-        $doc_rfca = $data_informacion['doc_rfc']; // Guarda el archivo RFC del docente
+        $doc_rfc = $data_informacion['doc_rfc']; // Guarda el nombre del archivo RFC del docente
         $nss = $data_informacion['nss']; // Guarda el numero de seguridad social del docente
+        $cve_docente = $data_informacion['cve_docente']; // Guarda la clave del docente vinculado
     }
-    // Consulta todos los datos de la tabla contacto
-    $sql_contacto = mysqli_query($conexion,"SELECT * FROM contacto WHERE cve_docente = $idDoc");
-    while ($data_contacto = mysqli_fetch_array($sql_contacto))
+    $sql_contacto = mysqli_query($conexion,"SELECT * FROM contacto WHERE cve_docente = $idDoc"); // Consulta todos los datos de la tabla contacto
+    while ($data_contacto = mysqli_fetch_array($sql_contacto)) // Bucle para almacenar los datos de la tabla contacto
     {
         $cve_contacto = $data_contacto['cve_contacto']; // Guarda la clave de contacto
         $correo_ins = $data_contacto['correo_ins']; // Guarda el correo institucional del docente
         $correo_per = $data_contacto['correo_per']; // Guarda el correo personal del docente
         $telefono = $data_contacto['telefono']; // Guarda el numero de telefono del docente
     }
-    // Consulta todos los datos de la tabla domicilio
-    $sql_domicilio = mysqli_query($conexion,"SELECT * FROM domicilio WHERE cve_docente = $idDoc");
-    while ($data_domicilio = mysqli_fetch_array($sql_domicilio))
+    $sql_domicilio = mysqli_query($conexion,"SELECT * FROM domicilio WHERE cve_docente = $idDoc"); // Consulta todos los datos de la tabla domicilio
+    while ($data_domicilio = mysqli_fetch_array($sql_domicilio)) // Bucle para almacenar los datos de la tabla domicilio
     {
         $cve_domicilio = $data_domicilio['cve_domicilio']; // Guarda la clave de domicilio
         $calle = $data_domicilio['calle']; // Guarda el nombre de la calle
@@ -70,63 +66,211 @@ if($result_sql_docente == 0){ // Verifica que la cantidad no este vacia
         $ciudad = $data_domicilio['ciudad']; // Guarda el nombre de la ciudad
         $estado = $data_domicilio['estado']; // Guarda el nombre del estado
         $pais = $data_domicilio['pais']; // Guarda el nombre del pais
-        $doc_doma = $data_domicilio['doc_dom']; // Guarda el nombre del comprobante de domicilio
+        $doc_dom = $data_domicilio['doc_dom']; // Guarda el nombre del comprobante de domicilio
     }
-    // Consulta todos los datos de la tabla viaje
-    $sql_viaje = mysqli_query($conexion,"SELECT * FROM viaje WHERE cve_docente = $idDoc");
-    while ($data_viaje = mysqli_fetch_array($sql_viaje))
+    $sql_viaje = mysqli_query($conexion,"SELECT * FROM viaje WHERE cve_docente = $idDoc"); // Consulta todos los datos de la tabla viaje
+    while ($data_viaje = mysqli_fetch_array($sql_viaje)) // Bucle para almacenar los datos de la tabla viaje
     {
         $cve_viaje = $data_viaje['cve_viaje']; // Guarda la clave de viaje
         $disp_viaje = $data_viaje['disp_viaje']; // Guarda la disponibilidad de viaje
         $num_pasaporte = $data_viaje['num_pasaporte']; // Guarda el numero de pasaporte del docente
         $fecha_ven_pas = $data_viaje['fecha_ven_pas']; // Guarda la fecha de vencimiento del pasaporte
     }
+    // Consulta todos los datos de Formacion
+    $sql_formacion = mysqli_query($conexion, "SELECT * FROM formacion WHERE activo = 1 AND cve_docente = $idDoc");
+    $contadorFO = 1; // Contador de Formacion
+    // Consulta todos los datos de Experiencia
+    $sql_experiencia = mysqli_query($conexion, "SELECT * FROM experiencia WHERE activo = 1 AND cve_docente = $idDoc");
+    $contadorEX = 1; // Contador de Experiencia
+    // Consulta todos los datos de Premios
+    $sql_premio = mysqli_query($conexion, "SELECT * FROM premios WHERE activo = 1 AND cve_docente = $idDoc");
+    $contadorPR = 1; // Contador de Premios
+    // Consulta todos los datos de Publicaciones
+    $sql_publicacion = mysqli_query($conexion, "SELECT * FROM publicaciones WHERE activo = 1 AND cve_docente = $idDoc");
+    $contadorPU = 1; // Contador de Publicaciones
 }
-
 // Almacena los datos en un arreglo(array)
-$array = Array (
-    "cve_docente" => "$idDoc",
-    "puesto" => "$puesto",
-    "nombre" => "$nombre",
-    "apellido1" => "$apellido1",
-    "apellido2" => "$apellido2",
-    "foto" => "$foto",
-    "institucion" => "$institucion",
-    "tipo_contratacion" => "$tipo_contratacion",
-    "fecha_ingreso" => "$fecha_ingreso",
-    "num_empleado" => "$num_empleado",
-    "cuenta" => "$cuenta",
-    "fecha_add" => "$fecha_add",
-    "user_cve" => "$user_cve",
-    "activo" => "$activo",
-    "formacion" => Array (
-        "id" => "02",
-        "name" => "Jennifer Laurence",
-        "designation" => "Senior Programmer"
+$arrayDA = Array (
+    "Datos" => Array (
+        "cve_docente" => "$idDoc",
+        "puesto" => "$puesto",
+        "nombre" => "$nombre",
+        "apellido1" => "$apellido1",
+        "apellido2" => "$apellido2",
+        "foto" => "$foto",
+        "institucion" => "$institucion",
+        "tipo_contratacion" => "$tipo_contratacion",
+        "fecha_ingreso" => "$fecha_ingreso",
+        "num_empleado" => "$num_empleado",
+        "cuenta" => "$cuenta",
+        "fecha_add" => "$fecha_add",
+        "user_cve" => "$user_cve",
+        "activo" => "$activo"
     ),
-    "experiencia" => Array(
-        "id" => "03",
-        "name" => "Medona Oliver",
-        "designation" => "Office Manager"
+    "Informacion" => Array (
+        "cve_info" => "$cve_info",
+        "fecha_nac" => "$fecha_nac",
+        "doc_nac" => "$doc_nac",
+        "genero" => "$genero",
+        "estado_civil" => "$estado_civil",
+        "nacionalidad" => "$nacionalidad",
+        "curp" => "$curp",
+        "doc_curp" => "$doc_curp",
+        "rfc" => "$rfc",
+        "doc_rfc" => "$doc_rfc",
+        "nss" => "$nss",
+        "cve_docente" => "$cve_docente"
+    ),
+    "Contacto" => Array (
+        "cve_contacto" => "$cve_contacto",
+        "correo_ins" => "$correo_ins",
+        "correo_per" => "$correo_per",
+        "telefono" => "$telefono",
+        "cve_docente" => "$cve_docente"
+    ),
+    "Domicilio" => Array (
+        "cve_domicilio" => "$cve_domicilio",
+        "calle" => "$calle",
+        "num_ext" => "$num_ext",
+        "num_int" => "$num_int",
+        "codigo_postal" => "$codigo_postal",
+        "colonia" => "$colonia",
+        "municipio" => "$municipio",
+        "ciudad" => "$ciudad",
+        "estado" => "$estado",
+        "pais" => "$pais",
+        "doc_dom" => "$doc_dom",
+        "cve_docente" => "$cve_docente"
+    ),
+    "Viaje" => Array (
+        "cve_viaje" => "$cve_viaje",
+        "disp_viaje" => "$disp_viaje",
+        "num_pasaporte" => "$num_pasaporte",
+        "fecha_ven_pas" => "$fecha_ven_pas",
+        "cve_docente" => "$cve_docente"
     )
 );
+// Almacena la formacion en un arreglo(array)
+$arrayFO = Array (); // El arreglo vacio donde se almacenaran los ciclos del bucle
+while ($data_formacion = mysqli_fetch_array($sql_formacion)) // Crea un bucle para mostrar los resultados
+{
+    array_push($arrayFO,
+        array("Formacion" => Array (
+            "$contadorFO" => Array (
+                'cve_formacion'=>$data_formacion['cve_formacion'],
+                'nivel_estudio'=>$data_formacion['nivel_estudio'],
+                'siglas_estudio'=>$data_formacion['siglas_estudio'],
+                'institucion'=>$data_formacion['institucion'],
+                'area'=>$data_formacion['area'],
+                'disciplina'=>$data_formacion['disciplina'],
+                'pais'=>$data_formacion['pais'],
+                'fecha_inicio'=>$data_formacion['fecha_inicio'],
+                'fecha_fin'=>$data_formacion['fecha_fin'],
+                'fecha_titulacion'=>$data_formacion['fecha_titulacion'],
+                'habilidades'=>$data_formacion['habilidades'],
+                'cve_docente'=>$data_formacion['cve_docente'],
+                'fecha_add'=>$data_formacion['fecha_add'],
+                'user_cve'=>$data_formacion['user_cve'],
+                'activo'=>$data_formacion['activo']
+            )
+        ))
+    );
+    $contadorFO = $contadorFO + 1; // Incrementa el contador
+}
+// Almacena la experiencia en un arreglo(array)
+$arrayEX = Array (); // El arreglo vacio donde se almacenaran los ciclos del bucle
+while ($data_experiencia = mysqli_fetch_array($sql_experiencia)) // Crea un bucle para mostrar los resultados
+{
+    array_push($arrayEX,
+        array("Experiencia" => Array (
+            "$contadorEX" => Array (
+                'cve_experiencia'=>$data_experiencia['cve_experiencia'],
+                'actividad'=>$data_experiencia['actividad'],
+                'institucion'=>$data_experiencia['institucion'],
+                'periodo'=>$data_experiencia['periodo'],
+                'intereses'=>$data_experiencia['intereses'],
+                'cve_docente'=>$data_experiencia['cve_docente'],
+                'fecha_add'=>$data_experiencia['fecha_add'],
+                'user_cve'=>$data_experiencia['user_cve'],
+                'activo'=>$data_experiencia['activo']
+            )
+        ))
+    );
+    $contadorEX = $contadorEX + 1; // Incrementa el contador
+}
+// Almacena los premios en un arreglo(array)
+$arrayPR = Array (); // El arreglo vacio donde se almacenaran los ciclos del bucle
+while ($data_premio = mysqli_fetch_array($sql_premio)) // Crea un bucle para mostrar los resultados
+{
+    array_push($arrayPR,
+        array("Premios" => Array (
+            "$contadorPR" => Array (
+                'cve_premio'=>$data_premio['cve_premio'],
+                'nombre'=>$data_premio['nombre'],
+                'fecha'=>$data_premio['fecha'],
+                'institucion'=>$data_premio['institucion'],
+                'motivo'=>$data_premio['motivo'],
+                'descripcion'=>$data_premio['descripcion'],
+                'cve_docente'=>$data_premio['cve_docente'],
+                'fecha_add'=>$data_premio['fecha_add'],
+                'user_cve'=>$data_premio['user_cve'],
+                'activo'=>$data_premio['activo']
+            )
+        ))
+    );
+    $contadorPR = $contadorPR + 1; // Incrementa el contador
+}
+// Almacena las publicaciones en un arreglo(array)
+$arrayPU = Array (); // El arreglo vacio donde se almacenaran los ciclos del bucle
+while ($data_publicacion = mysqli_fetch_array($sql_publicacion)) // Crea un bucle para mostrar los resultados
+{
+    array_push($arrayPU,
+        array("Publicaciones" => Array (
+            "$contadorPU" => Array (
+                'cve_publicacion'=>$data_publicacion['cve_publicacion'],
+                'tipo'=>$data_publicacion['tipo'],
+                'autor'=>$data_publicacion['autor'],
+                'titulo_articulo'=>$data_publicacion['titulo_articulo'],
+                'titulo_revista'=>$data_publicacion['titulo_revista'],
+                'pagina_inicio'=>$data_publicacion['pagina_inicio'],
+                'pagina_fin'=>$data_publicacion['pagina_fin'],
+                'pais'=>$data_publicacion['pais'],
+                'editorial'=>$data_publicacion['editorial'],
+                'volumen'=>$data_publicacion['volumen'],
+                'fecha_publicacion'=>$data_publicacion['fecha_publicacion'],
+                'proposito'=>$data_publicacion['proposito'],
+                'estado'=>$data_publicacion['estado'],
+                'cve_docente'=>$data_publicacion['cve_docente'],
+                'fecha_add'=>$data_publicacion['fecha_add'],
+                'user_cve'=>$data_publicacion['user_cve'],
+                'activo'=>$data_publicacion['activo']
+            )
+        ))
+    );
+    $contadorPU = $contadorPU + 1; // Incrementa el contador
+}
+// Almacena el conjunto de arreglos dentro de otro arreglo
+$array = Array(
+    $arrayDA,
+    $arrayFO,
+    $arrayEX,
+    $arrayPR,
+    $arrayPU
+);
+//echo json_encode($array, JSON_FORCE_OBJECT);
 // Transforma el arreglo en el archivo JSON
 $json = json_encode($array);
 $bytes = file_put_contents("".$num_empleado.".json", $json);
 // Mueve el archivo a la locacion correcta
-$archivo = "".$num_empleado.".json";
+$archivo = "".$num_empleado.".json"; // Nombre del archivo
 $lugarActual = $archivo;
 $lugarNuevo = "../files/docente/json/".$archivo;
 $mover = rename($lugarActual, $lugarNuevo);
-if(!$mover)
-{
-    echo "No se movio el archivo";
-}
-
-$rutaArchivo = "../files/docente/json/";
-$path = $rutaArchivo.$archivo;
+// Apartado de descarga
+$rutaArchivo = "../files/docente/json/"; // Almacena la ruta del archivo
+$path = $rutaArchivo.$archivo; // Direccion completa del archivo
 $type = '';
-
+// Condiciones para descargar
 if (is_file($path)) {
     $size = filesize($path);
     if (function_exists('mime_content_type')) {
@@ -154,5 +298,5 @@ if (is_file($path)) {
 ?>
 <?php
 // --- Pagina[expDocente] (Prototipo) ---
-// Ultima modificacion -- [13/07/2022 (12:00 hrs)]
+// Ultima modificacion -- [22/07/2022 (11:04 hrs)]
 ?>
--- sistema/secciones/infDocente.php
+++ sistema/secciones/infDocente.php
@@ -460,7 +460,7 @@ if($result_sql_docente == 0){ // Verifica que la cantidad no este vacia
                                         <?php
                                             //Paginador
                                             include "../config/conexion.php";
-                                            $sql_formacion = mysqli_query($conexion, "SELECT COUNT(*) AS tr_formacion FROM formacion WHERE activo = 1"); // Cuenta y almacena todos los registros activos
+                                            $sql_formacion = mysqli_query($conexion, "SELECT COUNT(*) AS tr_formacion FROM formacion WHERE activo = 1 AND cve_docente = $idDoc"); // Cuenta y almacena todos los registros activos
                                             $result_formacion = mysqli_fetch_array($sql_formacion);
                                             $total_formacion = $result_formacion['tr_formacion'];
                                             $por_pagina = 5; // Indicador de cuantos registros mostrara por pagina
@@ -473,7 +473,7 @@ if($result_sql_docente == 0){ // Verifica que la cantidad no este vacia
                                             $desde = ($pagina-1) * $por_pagina; //Identifica la posicion de la pagina
                                             $total_pagina = ceil($total_formacion / $por_pagina); // Calcula el total de las paginas
                                             // Realiza la consulta de los datos a mostrar en la lista
-                                            $query = mysqli_query($conexion, "SELECT * FROM formacion WHERE activo = 1 ORDER BY cve_formacion ASC LIMIT $desde,$por_pagina;");
+                                            $query = mysqli_query($conexion, "SELECT * FROM formacion WHERE activo = 1 AND cve_docente = $idDoc ORDER BY cve_formacion ASC LIMIT $desde,$por_pagina;");
                                             mysqli_close($conexion); // Cierra la conexion a la bd
                                             $result = mysqli_num_rows($query); // Calcula el numero de filas de la consulta
                                             if($result > 0){ // Valida si el numero de consultas es mayor a cero
@@ -502,49 +502,49 @@ if($result_sql_docente == 0){ // Verifica que la cantidad no este vacia
                                                 }
                                             }
                                             ?>
-                                        </table>
-                                        <?php
+                                            </table>
+                                            <?php
                                             if($total_formacion!= 0){ // Almacena el total de registros de Formacion
-                                        ?>
-                                        <div>
-                                            <ul class="pagination justify-content-end">
-                                                <?php
-                                                    if($pagina != 1)
-                                                    { ?>
-                                                        <li class="page-item"><a class="page-link" href="?pagina=<?php echo 1; ?>">|<</a></li>
-                                                        <li class="page-item"><a class="page-link" href="?pagina=<?php echo $pagina-1; ?>"><<</a></li>
-                                                <?php } ?>
-                                                <?php
-                                                    for ($i=1; $i <= $total_pagina; $i++){
-                                                        if($i == $pagina){
-                                                            echo '<li class="page-item active"><a class="page-link">'.$i.'</a></li>';
-                                                        }else{
-                                                            echo '<li class="page-item"><a class="page-link" href="?pagina='.$i.'">'.$i.'</a></li>';
+                                            ?>
+                                                <div>
+                                                    <ul class="pagination justify-content-end">
+                                                        <?php
+                                                        if($pagina != 1)
+                                                        { ?>
+                                                            <li class="page-item"><a class="page-link" href="?pagina=<?php echo 1; ?>">|<</a></li>
+                                                            <li class="page-item"><a class="page-link" href="?pagina=<?php echo $pagina-1; ?>"><<</a></li>
+                                                        <?php } ?>
+                                                        <?php
+                                                        for ($i=1; $i <= $total_pagina; $i++){
+                                                            if($i == $pagina){
+                                                                echo '<li class="page-item active"><a class="page-link">'.$i.'</a></li>';
+                                                            }else{
+                                                                echo '<li class="page-item"><a class="page-link" href="?pagina='.$i.'">'.$i.'</a></li>';
+                                                            }
                                                         }
-                                                    }
-                                                ?>
-                                                <?php
-                                                    if($pagina != $total_pagina)
-                                                    { ?>
-                                                        <li class="page-item"><a class="page-link" href="?pagina=<?php echo $pagina+1; ?>">>></a></li>
-                                                        <li class="page-item"><a class="page-link" href="?pagina=<?php echo $total_pagina; ?>">>|</a></li>
-                                                <?php } ?>
-                                            </ul>
-                                        </div>
-                                        <?php }else{ ?>
-                                            <div class="alert alert-dismissible alert-light mx-auto"> <!-- Mensaje de que no hay registros -->
-                                                <h4 class="alert-heading text-center">No hay formacion registrada para este docente, aun...</h4>
-                                                <p class="mb-0 text-center">Agrega el primer registro</p>
-                                                <br>
-                                                <div class="text-center">
-                                                    <a role="button" class="btn btn-primary" href="nuevoFormacion.php?id_d=<?php echo $idDoc;?>" >Nuevo registro</a> <!-- Redirecciona para registrar Formacion -->
+                                                        ?>
+                                                        <?php
+                                                        if($pagina != $total_pagina)
+                                                        { ?>
+                                                            <li class="page-item"><a class="page-link" href="?pagina=<?php echo $pagina+1; ?>">>></a></li>
+                                                            <li class="page-item"><a class="page-link" href="?pagina=<?php echo $total_pagina; ?>">>|</a></li>
+                                                        <?php } ?>
+                                                    </ul>
                                                 </div>
+                                            <?php }else{ ?>
+                                                <div class="alert alert-dismissible alert-light mx-auto"> <!-- Mensaje de que no hay registros -->
+                                                    <h4 class="alert-heading text-center">No hay formacion registrada para este docente, aun...</h4>
+                                                    <p class="mb-0 text-center">Agrega el primer registro</p>
+                                                    <br>
+                                                    <div class="text-center">
+                                                        <a role="button" class="btn btn-primary" href="nuevoFormacion.php?id_d=<?php echo $idDoc;?>" >Nuevo registro</a> <!-- Redirecciona para registrar Formacion -->
+                                                    </div>
+                                                </div>
+                                            <?php } ?>
+                                            <div class="text-center">
+                                                <br>
+                                                <button type="submit" name="decision" value="volver" class="btn btn-danger" style="float: center;">Volver</button> <!-- Boton para volver -->
                                             </div>
-                                        <?php } ?>
-                                    <div class="text-center">
-                                        <br>
-                                        <button type="submit" name="decision" value="volver" class="btn btn-danger" style="float: center;">Volver</button> <!-- Boton para volver -->
-                                    </div>
                                 </div>
                             </div>
                         </div>
