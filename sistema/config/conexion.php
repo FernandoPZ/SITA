73099fe92b6a00d4de7e7fa516681a5bdce8cf89
Fernando <50506780+FernandoPZ@users.noreply.github.com>
2022-08-31 12:31:21 -0500

    Conexión externa de la bd

--- sistema/config/conexion.php
+++ sistema/config/conexion.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost'; //Guarda el nombre del host
-$user = 'root'; //Guarda el nombre del usuario
-$password = ''; //Guarda la contraseña
-$db = 'sita'; //Guarda el nombre de la base de datos
+$host = 'us-cdbr-east-06.cleardb.net'; //Guarda el nombre del host
+$user = 'b754efcbe8d70d'; //Guarda el nombre del usuario
+$password = '33d909b3'; //Guarda la contraseña
+$db = 'heroku_9c1f9dafb8e900c'; //Guarda el nombre de la base de datos
 $conexion = @mysqli_connect($host,$user,$password,$db); //Realiza la conexion
 // --- Conexion con la bd ---
 // Ultima modificacion -- [22/07/2022 (12:10 hrs)]
